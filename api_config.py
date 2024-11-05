07f74a65b4b27a8a9e17e2908bea4148717a3a1c
guangxiangdebizi <154864206+guangxiangdebizi@users.noreply.github.com>
2024-11-05 18:27:47 +0800

    Update api_config.py

--- api_config.py
+++ api_config.py
@@ -2,10 +2,10 @@ from openai import OpenAI
 
 # 配置OpenAI客户端
 client = OpenAI(
-    api_key="962da72c3cb62b09521923cfefabafc6.oyAzeB72sEIJpWDr",
-    base_url="https://open.bigmodel.cn/api/paas/v4/"
+    api_key="Your_Api_Key",
+    base_url="Your_Api_Url"
 )
 
 # 模型名称
-MODEL_NAME = "GLM-4-flash"
+MODEL_NAME = "Your_Selected_Model"
 
